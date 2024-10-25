1419b9aaf781d63efeb54cc9b8a52f55e2eeb142
Muutassim Mukhtar <122842090+triplemcoder14@users.noreply.github.com>
2024-10-25 15:11:46 +0100

    add helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
+// StrOrDefault returns the value of the string pointer or a default value if it's nil.
 func StrOrDefault(s *string, defaultVal string) string {
 	if s == nil {
 		return defaultVal
@@ -11,6 +12,7 @@ func StrOrDefault(s *string, defaultVal string) string {
 	return *s
 }
 
+// GetTagName retrieves the "Name" tag of an EC2 instance.
 func GetTagName(inst *types.Instance) string {
 	var nameValue string
 	for _, t := range inst.Tags {
@@ -21,17 +23,8 @@ func GetTagName(inst *types.Instance) string {
 	return nameValue
 }
 
-func filter(slice []string, predicate func(string) bool) []string {
-	var result []string
-	for _, elem := range slice {
-		if predicate(elem) {
-			result = append(result, elem)
-		}
-	}
-	return result
-}
-
-func contains(slice []string, elem string) bool {
+// Contains checks if a slice contains a specific element.
+func Contains(slice []string, elem string) bool {
 	for _, v := range slice {
 		if v == elem {
 			return true
@@ -39,3 +32,49 @@ func contains(slice []string, elem string) bool {
 	}
 	return false
 }
+
+
+
+
+
+// package helpers
+
+// import (
+// 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+// )
+
+// func StrOrDefault(s *string, defaultVal string) string {
+// 	if s == nil {
+// 		return defaultVal
+// 	}
+// 	return *s
+// }
+
+// func GetTagName(inst *types.Instance) string {
+// 	var nameValue string
+// 	for _, t := range inst.Tags {
+// 		if *t.Key == "Name" {
+// 			nameValue = *t.Value
+// 		}
+// 	}
+// 	return nameValue
+// }
+
+// func filter(slice []string, predicate func(string) bool) []string {
+// 	var result []string
+// 	for _, elem := range slice {
+// 		if predicate(elem) {
+// 			result = append(result, elem)
+// 		}
+// 	}
+// 	return result
+// }
+
+// func contains(slice []string, elem string) bool {
+// 	for _, v := range slice {
+// 		if v == elem {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
