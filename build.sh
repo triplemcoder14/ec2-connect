52ce9a68fc0e13df910e3fbe6e1da0a974b91df7
root <mukhy16@gmail.com>
2024-10-26 19:38:06 +0100

    define versions and GOARCH

--- build.sh
+++ build.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-# Add Go to the PATH
+#Author: Muutassim-Mukhtar<mukhy16@gmail.com>
+
+# add Go to the PATH
 export PATH=$PATH:/usr/local/go/bin
 
-# Check if Go is installed
+# check if Go is installed
 if ! command -v go &> /dev/null; then
     echo "Go could not be found. Please install Go."
     exit 1
 fi
 
-# Create a release directory
+# create a release directory
 mkdir -p release
 
-# Define versions and architectures
+# define versions and architectures
 VERSION="v1.0.0"
 PLATFORMS=("darwin/amd64" "darwin/arm64" "linux/386" "linux/amd64" "linux/arm64" "windows/386" "windows/amd64" "windows/arm64")
 
-# Build for each platform
+# build for each platform
 for PLATFORM in "${PLATFORMS[@]}"; do
     OS="${PLATFORM%/*}"
     ARCH="${PLATFORM#*/}"
     OUTPUT="release/ec2-ssh_${VERSION}-${OS}-${ARCH}"
 
-    # Set GOOS and GOARCH for cross-compilation
+    # set GOoS and goarch for cross-compilation
     echo "Building for $OS/$ARCH..."
     GOOS="$OS" GOARCH="$ARCH" go build -o "$OUTPUT" ./initssh.go
 
-    # Check if the binary was created successfully
+    # check if the binary was created successfully
     if [ ! -f "$OUTPUT" ]; then
         echo "Failed to create binary for $OS/$ARCH"
         exit 1
     fi
 
-    # Package the binary
+    # package the binary
     tar -czvf "${OUTPUT}.tar.gz" -C release "$OUTPUT"
 done
 
